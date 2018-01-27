712ef541b1ffdd5e9dd571f96d6f7d67fd8b5c1e
William Amato <IamWill216@yahoo.com>
2018-01-27 10:00:13 -0500

    altered port and function

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var port = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -91,6 +92,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
